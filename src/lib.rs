fed1660a2fd22f430e65b126bdebfdfddcd2d2dc
xephyris <114379647+GoeyGreen@users.noreply.github.com>
2025-10-04 23:29:52 -0700

    reformat: remove old methods

--- src/lib.rs
+++ src/lib.rs
@@ -65,19 +65,11 @@ pub fn get_accent_color() -> Result<Color, Error>{
     }
 }
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 
     #[test]
     fn get_rgb() {
